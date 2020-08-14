8ee6edf0847ec8ff56e98167137284b848811533
Mohammed Hamdi <mhah000@hotmail.com>
2020-08-14 13:39:14 +0200

    Create message model, and custom message cell.

---
+++ Flash-Chat/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash-Chat
+//
+//  Created by Mohammed Hamdi on 8/14/20.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
