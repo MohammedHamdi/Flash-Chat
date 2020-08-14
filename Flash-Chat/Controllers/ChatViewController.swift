c609113103a50dad6da54012329b451f1e65b782
Mohammed Hamdi <mhah000@hotmail.com>
2020-08-14 16:44:55 +0200

    Update the chatview in real-time

--- Flash-Chat/Controllers/ChatViewController.swift
+++ Flash-Chat/Controllers/ChatViewController.swift
@@ -32,9 +32,10 @@ class ChatViewController: UIViewController {
     }
     
     func loadMessages() {
-        messages = []
         
-        db.collection(K.FStore.collectionName).getDocuments { (querySnapshot, error) in
+        db.collection(K.FStore.collectionName).addSnapshotListener { (querySnapshot, error) in
+            self.messages = []
+            
             if let error = error {
                 print("There was an issue retrieving data from FIrestore: ", error)
             } else {
